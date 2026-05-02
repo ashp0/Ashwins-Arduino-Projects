c3addcb9624df288310c1bb838aa2995d13cbaf1
Ashwin Paudel <ashwinp.res0@gmail.com>
2026-05-02 10:42:23 -0400

    Pressure: barometre formula + low filter bypass

--- src/pressureSensor.cpp
+++ src/pressureSensor.cpp
@@ -5,12 +5,31 @@
 #include <SPI.h>
 #include <Wire.h>
 
-int screen_width = 128;
-int screen_height = 64;
-int OLED_reset = -1;
+const int configure_base_pin = 6;
+bool lastButtonState = HIGH;
+unsigned long lastDebounceTime = 0;
+const unsigned long BUTTON_DEBOUNCE_MS = 10;
+
+float base_pressure;
+bool base_pressure_set = false;
+float height_filtered = 0;
+
+unsigned long lastDebounceTimeBMP = 0;
+const unsigned long BMP_SAMPLE_INTERVAL_MS = 693;
+
+// Constants
+constexpr float TEMP_AT_SEA_LEVEL = 288.15f;			   // K (15°C)
+constexpr float MOLAR_MASS_OF_AIR = 0.0289644f;			   // kg/mol
+constexpr float ACCELERATION_GRAVITY = 9.80665f;		   // m/s^2
+constexpr float UNIVERSAL_GAS_CONSTANT = 8.3144598f;	   // J/(mol·K)
+constexpr float TEMPERATURE_CHANGE_BY_ALTITUDE = 0.0065f;  // K/m
+
+const int SCREEN_WIDTH = 128;
+const int SCREEN_HEIGHT = 64;
+const int OLED_RESET_PIN = -1;
 #define SCREEN_ADDRESS 0x3C
 
-Adafruit_SSD1306 myDisplay(screen_width, screen_height, &Wire, OLED_reset);
+Adafruit_SSD1306 myDisplay(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET_PIN);
 
 Adafruit_BMP085 myBMP;
 
@@ -25,6 +44,14 @@ float altitude;
 
 // Whether the device is connected or not.
 bool bmp_connected;
+
+float computeChangeInAltitude(float currentPressure, float basePressure) {
+	float left_term = TEMP_AT_SEA_LEVEL / TEMPERATURE_CHANGE_BY_ALTITUDE;
+	float power = (UNIVERSAL_GAS_CONSTANT * TEMPERATURE_CHANGE_BY_ALTITUDE) /
+				  (MOLAR_MASS_OF_AIR * ACCELERATION_GRAVITY);
+	float pressure_ratio = pow(currentPressure / basePressure, power);
+	return left_term * (1 - pressure_ratio);
+}
 
 void setup() {
 	Serial.begin(9600);
@@ -42,29 +69,62 @@ void setup() {
 	String message;
 
 	if (bmp_connected) {
-		message = "Found BMP Device!";
+		message = F("Found BMP Device!");
 	} else {
-		message = "Error: Unable to find the BMP sensor or unable to begin.";
+		message = F("Error: Unable to find the BMP sensor or unable to begin.");
 	}
 
 	myDisplay.println(message);
 	myDisplay.display();
+
+	pinMode(configure_base_pin, INPUT_PULLUP);
 }
 
 void loop() {
-	temperature = myBMP.readTemperature();
-	air_pressure = myBMP.readPressure();
-	altitude = myBMP.readAltitude();
+	if (!bmp_connected)
+		return;
 
-	myDisplay.setCursor(0, 0);
-	myDisplay.clearDisplay();
-	myDisplay.print(temperature);
-	myDisplay.println(" C");
-	myDisplay.print(altitude);
-	myDisplay.println(" m");
-	myDisplay.print(air_pressure);
-	myDisplay.println(" Pa");
-	myDisplay.display();
+	bool didPressReconfigureButton = digitalRead(configure_base_pin);
+	unsigned long millis_time = millis();
 
-	delay(2000);
+	if (didPressReconfigureButton != lastButtonState) {
+		if (millis_time - lastDebounceTime > BUTTON_DEBOUNCE_MS) {
+			if (didPressReconfigureButton == LOW) {
+				base_pressure = air_pressure;
+				base_pressure_set = true;
+			}
+			lastDebounceTime = millis_time;
+		}
+	}
+	lastButtonState = didPressReconfigureButton;
+
+	if (millis_time - lastDebounceTimeBMP > BMP_SAMPLE_INTERVAL_MS) {
+		temperature = myBMP.readTemperature();
+		air_pressure = myBMP.readPressure();
+		altitude = myBMP.readAltitude();
+
+		myDisplay.setCursor(0, 0);
+		myDisplay.clearDisplay();
+		myDisplay.print(temperature);
+		myDisplay.println(" C");
+		myDisplay.print(altitude);
+		myDisplay.println(" m");
+		myDisplay.print(air_pressure);
+		myDisplay.println(" Pa");
+
+		if (base_pressure_set) {
+			myDisplay.println("--------");
+			myDisplay.print("h: ");
+			float height_now = computeChangeInAltitude(air_pressure, base_pressure);
+
+			// Low pass filter
+			height_filtered = 0.98 * height_filtered + 0.02 * height_now;
+
+			myDisplay.print(height_filtered);
+			myDisplay.println(" m");
+		}
+
+		lastDebounceTimeBMP = millis_time;
+		myDisplay.display();
+	}
 }
